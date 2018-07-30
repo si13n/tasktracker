e3078b899488d2bf5da3ec1af3fb56f547627467
Serhii Domin <sergeydomin@gmail.com>
2018-07-30 15:32:10 +0300

    Search for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        @users = User.all
+        @users = User.where(nil)
+        @users = @users.search(params)
         @team_names = Team.all.collect {|team| [ team.name, team.id ] }
-        @team_names.unshift("All teams")
+        @team_names.unshift(nil)
     end
 
     def delete_avatar
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,19 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  #cope :email, -> (email) { where("email LIKE ?", "#{email}%")}
+  #scope :names, -> (name) { where("name LIKE ?", "%#{name}%")}
+  #scope :recent, lambda { :conditions => ['updated_at > ? AND admin != ?', 5.minutes.ago, true] }
+
+  scope :search, -> (params) do
+        where("email LIKE ?", "%#{params[:email]}%").
+        where("name LIKE ?", "#{params[:name]}%").
+        where("role LIKE ?", "#{params[:role]}%").
+        where(team_id: params[:team]).
+        where(active: true)
+  end
+
   def online?
     self.updated_at > 10.minutes.ago
   end
-
 end
